cd45fa68f9ece02462169df59d59d5b2768ecddc
igor-ribeiiro <ribeiro_igor@hotmail.com>
2018-09-25 23:07:22 -0300

    Adding check equivalents funcion

--- projeto_1/state_reducer.py
+++ projeto_1/state_reducer.py
@@ -2,12 +2,42 @@ class StateReducer:
     def __init__(self, graph):
         self.graph = graph
 
-    def check_equivalent_node(self, node_1, node_2):
-        are_equivalent = True
-
+    def get_edges_that_begin_in_node(self, node):
+        edges = []
         for edge in self.graph.edges:
-            pass
-        return are_equivalent
+            if str(edge.id_1) == str(node.id):
+                edges.append(edge)
+        return edges
+
+    def get_edges_that_end_in_node(self, node):
+        edges = []
+        for edge in self.graph.edges:
+            if str(edge.id_2) == str(node.id):
+                edges.append(edge)
+        return edges
+
+    def are_two_edges_equal(self, edges_1, edges_2):
+        if len(edges_1) != len(edges_2):
+            return False
+
+        equal = True
+        for edge_1 in edges_1:
+            edge_equal = False
+            for edge_2 in edges_2:
+                if edge_1.id_1 == edge_2.id1 and edge_1.id2 == edge_2.id2 and edge_1.label == edge_2.label:
+                    edge_equal = True
+                    break
+            if not edge_equal:
+                equal = False
+                break
+        return equal
+
+    def check_equivalent_node(self, node_1, node_2):
+        begin_equal = self.are_two_edges_equal(self.get_edges_that_begin_in_node(node_1),
+                                               self.get_edges_that_begin_in_node(node_2))
+        end_equal = self.are_two_edges_equal(self.get_edges_that_end_in_node(node_1),
+                                             self.get_edges_that_end_in_node(node_2))
+        return begin_equal and end_equal
 
     def merge_nodes(self, node_1, node_2):
         pass
@@ -29,6 +59,5 @@ class StateReducer:
                     if equivalents:
                         has_at_least_one_change = True
                         self.merge_nodes(node_1, node_2)
-
 
         return self.graph
